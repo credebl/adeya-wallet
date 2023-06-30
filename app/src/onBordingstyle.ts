3415df621117462714d2a58535313175dfadbd41
“poonam.ghewande@ayanworks.com” <poonam.ghewande@ayanworks.com>
2023-06-30 14:48:06 +0530

    Updated comented  Design changes  onboarding pages
    Signed-off-by: “poonam.ghewande@ayanworks.com” <“poonam.ghewande@ayanworks.com”>

---
+++ app/src/onBordingstyle.ts
@@ -0,0 +1,51 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    paddingTop: 50,
+  },
+  headerText: {
+    alignSelf: 'center',
+    marginBottom: 25,
+  },
+  backgroundImage: {
+    height: 245,
+    width: 343,
+    alignSelf: 'center',
+  },
+  Image: {
+    width: 78,
+    height: 78,
+    flexShrink: 0,
+    position: 'absolute',
+    right: 100,
+    top: 70,
+    left: 132,
+    bottom: 0,
+  },
+  qrImage: {
+    width: 81,
+    height: 82,
+    flexShrink: 0,
+    position: 'absolute',
+    right: 100,
+    top: 100,
+    left: 170,
+  },
+  bodyText: {
+    marginTop: 25,
+    display: 'flex',
+    width: 345,
+    flexDirection: 'column',
+    flexShrink: 0,
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#2289F7',
+  },
+  descriptiionText: { alignSelf: 'center', marginHorizontal: 20 },
+  startedButtonconatiner: {
+    margin: 25,
+  },
+  guideimages: { alignItems: 'center' },
+})
