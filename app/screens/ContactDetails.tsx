7e8c7517bec852a8c86713e07a4b39ee18955730
Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
2023-08-08 12:19:11 +0530

    feat: new app setup (#44)
    * refactor: delete old app architecture
    
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
    
    * refactor: delete old app architecture
    
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
    
    * feat: new app setup
    
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
    
    ---------
    
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>

---
+++ app/screens/ContactDetails.tsx
@@ -0,0 +1,118 @@
+import { CredentialState } from '@aries-framework/core'
+import { useAgent, useConnectionById, useCredentialByState } from '@aries-framework/react-hooks'
+import { useNavigation } from '@react-navigation/core'
+import { StackNavigationProp, StackScreenProps } from '@react-navigation/stack'
+import React, { useCallback, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import { DeviceEventEmitter } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import Toast from 'react-native-toast-message'
+
+import CommonRemoveModal from '../components/modals/CommonRemoveModal'
+import RecordRemove from '../components/record/RecordRemove'
+import { ToastType } from '../components/toast/BaseToast'
+import { EventTypes } from '../constants'
+import { useConfiguration } from '../contexts/configuration'
+import { BifoldError } from '../types/error'
+import { ContactStackParams, Screens, TabStacks } from '../types/navigators'
+import { Attribute } from '../types/record'
+import { ModalUsage } from '../types/remove'
+import { formatTime } from '../utils/helpers'
+
+type ContactDetailsProps = StackScreenProps<ContactStackParams, Screens.ContactDetails>
+
+const ContactDetails: React.FC<ContactDetailsProps> = ({ route }) => {
+  const { connectionId } = route?.params
+  const { agent } = useAgent()
+  const { t } = useTranslation()
+  const navigation = useNavigation<StackNavigationProp<ContactStackParams>>()
+  const [isRemoveModalDisplayed, setIsRemoveModalDisplayed] = useState<boolean>(false)
+  const [isCredentialsRemoveModalDisplayed, setIsCredentialsRemoveModalDisplayed] = useState<boolean>(false)
+  const connection = useConnectionById(connectionId)
+  // FIXME: This should be exposed via a react hook that allows to filter credentials by connection id
+  const connectionCredentials = [
+    ...useCredentialByState(CredentialState.CredentialReceived),
+    ...useCredentialByState(CredentialState.Done),
+  ].filter(credential => credential.connectionId === connection?.id)
+  const { record } = useConfiguration()
+
+  const handleOnRemove = () => {
+    if (connectionCredentials?.length) {
+      setIsCredentialsRemoveModalDisplayed(true)
+    } else {
+      setIsRemoveModalDisplayed(true)
+    }
+  }
+
+  const handleSubmitRemove = async () => {
+    try {
+      if (!(agent && connection)) {
+        return
+      }
+
+      await agent.connections.deleteById(connection.id)
+
+      navigation.navigate(Screens.Contacts)
+
+      // FIXME: This delay is a hack so that the toast doesn't appear until the modal is dismissed
+      await new Promise(resolve => setTimeout(resolve, 1000))
+
+      Toast.show({
+        type: ToastType.Success,
+        text1: t('ContactDetails.ContactRemoved'),
+      })
+    } catch (err: unknown) {
+      const error = new BifoldError(t('Error.Title1037'), t('Error.Message1037'), (err as Error).message, 1025)
+
+      DeviceEventEmitter.emit(EventTypes.ERROR_ADDED, error)
+    }
+  }
+
+  const handleCancelRemove = () => {
+    setIsRemoveModalDisplayed(false)
+  }
+
+  const handleGoToCredentials = () => {
+    navigation.getParent()?.navigate(TabStacks.CredentialStack, { screen: Screens.Credentials })
+  }
+
+  const handleCancelUnableRemove = () => {
+    setIsCredentialsRemoveModalDisplayed(false)
+  }
+
+  const callOnRemove = useCallback(() => handleOnRemove(), [])
+  const callSubmitRemove = useCallback(() => handleSubmitRemove(), [])
+  const callCancelRemove = useCallback(() => handleCancelRemove(), [])
+  const callGoToCredentials = useCallback(() => handleGoToCredentials(), [])
+  const callCancelUnableToRemove = useCallback(() => handleCancelUnableRemove(), [])
+
+  return (
+    <SafeAreaView style={{ flexGrow: 1 }} edges={['bottom', 'left', 'right']}>
+      {record({
+        fields: [
+          {
+            name: connection?.alias || connection?.theirLabel,
+            value: t('ContactDetails.DateOfConnection', {
+              date: connection?.createdAt ? formatTime(connection.createdAt) : '',
+            }),
+          },
+        ] as Attribute[],
+        footer: () => <RecordRemove onRemove={callOnRemove} />,
+      })}
+      <CommonRemoveModal
+        usage={ModalUsage.ContactRemove}
+        visible={isRemoveModalDisplayed}
+        onSubmit={callSubmitRemove}
+        onCancel={callCancelRemove}
+      />
+      <CommonRemoveModal
+        usage={ModalUsage.ContactRemoveWithCredentials}
+        visible={isCredentialsRemoveModalDisplayed}
+        onSubmit={callGoToCredentials}
+        onCancel={callCancelUnableToRemove}
+      />
+    </SafeAreaView>
+  )
+}
+
+export default ContactDetails
