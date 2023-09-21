414afba1ced3a21df9021b4f38579ae30ff0135c
Poonam Ghewande <poonam.ghewande@ayanworks.com>
2023-09-21 19:12:52 +0530

    refactor:firebase  ios config setup
    Signed-off-by: Poonam Ghewande <poonam.ghewande@ayanworks.com>

--- ios/AdeyaWallet/AppDelegate.mm
+++ ios/AdeyaWallet/AppDelegate.mm
@@ -2,6 +2,7 @@
 
 #import <React/RCTBundleURLProvider.h>
 
+#import <Firebase.h>
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
