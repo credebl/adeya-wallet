98aa42d00aeba8cd30bbdd7219660238d9190149
Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
2023-09-25 11:32:30 +0530

    chore: bump iOS version
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
    Signed-off-by: Poonam Ghewande <poonam.ghewande@ayanworks.com>

--- app/localization/en/index.ts
+++ app/localization/en/index.ts
@@ -136,7 +136,7 @@ const translation = {
     "Back": "Back",
     "Done": "Done",
   },
-  
+
   "Terms": {
     "TermsOfService": "Terms of Service",
     "TermsOfServiceText": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel consectetur diam. Nunc sit amet elit est. Praesent libero elit, consectetur dapibus diam non, facilisis euismod velit. Etiam a ligula eget leo elementum tincidunt. Fusce et lorem turpis. Nunc tempus nisl consectetur eros vehicula venenatis. Suspendisse potenti. Aenean vitae aliquet augue. Maecenas lacinia nunc vitae blandit hendrerit. Sed congue risus quis magna convallis sollicitudin. Integer in ante vel orci ornare porta quis id libero. Proin mollis urna nec lectus fringilla, sit amet aliquam urna fringilla. Praesent pellentesque non augue et gravida. Donec congue urna ac massa consequat, lacinia condimentum dolor blandit. Nam ultrices tellus at risus dignissim, quis cursus mauris pellentesque. Donec at scelerisque ipsum. Praesent eu massa at tellus cursus ornare. Fusce vel faucibus dolor. Etiam blandit velit sed velit tempus feugiat. Donec condimentum pretium suscipit. Sed suscipit, leo molestie tempus maximus, turpis enim hendrerit nibh, semper sagittis turpis velit sed nisl. Aliquam eu ultrices velit. Aenean tristique mauris justo, eu commodo quam semper non. Curabitur ultricies auctor mi eu tempus. Sed bibendum eros sed neque semper fermentum. Nullam porta tortor ut ante congue molestie. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur sit amet aliquam nunc, malesuada auctor quam. Pellentesque vel lobortis risus, volutpat suscipit velit. Aenean ut erat sed metus interdum mattis. Nam consectetur ante eu felis rhoncus, et volutpat dolor tincidunt. Vivamus sit amet feugiat mi. Proin in dui ac metus vehicula fringilla eget id mauris. Maecenas et elit venenatis dolor pulvinar pulvinar in et leo. Aliquam scelerisque viverra sapien at bibendum. Curabitur et libero nec enim convallis porttitor sed a libero. In hac habitasse platea dictumst. Integer dignissim velit eu pharetra ultricies. Vestibulum at velit hendrerit, pretium purus eget, lobortis tellus. Maecenas non erat ut lacus scelerisque luctus et et tellus.",
@@ -146,8 +146,8 @@ const translation = {
   "CameraDisclosure": {
     "AllowCameraUse": "Allow camera use",
     "CameraDisclosure": "The camera is used to scan QR codes that initiate a credential offer or credential request. No information about the images is stored, used for analytics, or shared.",
-    "ToContinueUsing": "To continue using the ADEYA scan feature, please allow camera permissions.",
-    "Allow": "Allow",
+    "ToContinueUsing": "To continue using the ADEYA Wallet scan feature, please allow camera permissions in the settings.",
+    "Continue": "Continue",
     "OpenSettings": "Open settings",
   },
   "PINCreate": {
@@ -218,7 +218,7 @@ const translation = {
     "BiometricsChangedEnterPIN": "Please enter your wallet PIN.",
     "AttemptLockoutWarning": "Note: for your security, entering another incorrect PIN will temporarily lock the wallet."
   },
-  
+
   "AttemptLockout": {
     "Title": "Your wallet is temporarily locked",
     "Description": "You have had too many unsuccessful PIN attempts.",
@@ -227,7 +227,7 @@ const translation = {
     "Minutes": "minutes",
     "Seconds": "seconds",
   },
-  
+
   "Biometry": {
     "Toggle": "Toggle Biometrics",
     "EnabledText1": "Use your phone's biometrics to log in rather than your wallet's PIN.",
@@ -654,8 +654,8 @@ const translation = {
     "you_have_successfully": 'You have successfully selected the words.',
     "complete_backup": 'Complete Backup',
     "backup_wallet": 'Backup Wallet',
-  
-  } 
+
+  }
 }
 
 export default translation
