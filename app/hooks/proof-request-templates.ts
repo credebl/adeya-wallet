d6d4a708a35fbed26ad2bfd4ceac340c4037c82e
Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
2023-11-24 12:11:32 +0530

    feat: add remote verification templates (#148)
    * feat: add remote verification templates
    
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
    
    * chore: bump versions
    
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
    
    * fix: verifier capability for previous version
    
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
    
    ---------
    
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
    Signed-off-by: Poonam Ghewande <poonam.ghewande@ayanworks.com>

--- app/hooks/proof-request-templates.ts
+++ app/hooks/proof-request-templates.ts
@@ -1,14 +1,36 @@
-import { useMemo } from 'react'
+import { useEffect, useState } from 'react'
 
 import { ProofRequestTemplate } from '../../verifier'
 import { useConfiguration } from '../contexts/configuration'
+import { applyTemplateMarkers, useRemoteProofBundleResolver } from '../utils/proofBundle'
 
 export const useTemplates = (): Array<ProofRequestTemplate> => {
-  const { proofRequestTemplates } = useConfiguration()
-  return proofRequestTemplates || []
+  const [proofRequestTemplates, setProofRequestTemplates] = useState<ProofRequestTemplate[]>([])
+  const { proofTemplateBaseUrl } = useConfiguration()
+  const resolver = useRemoteProofBundleResolver(proofTemplateBaseUrl)
+  const acceptDevCredentials = false
+  useEffect(() => {
+    resolver.resolve(acceptDevCredentials).then(templates => {
+      if (templates) {
+        setProofRequestTemplates(applyTemplateMarkers(templates))
+      }
+    })
+  }, [])
+  return proofRequestTemplates
 }
 
 export const useTemplate = (templateId: string): ProofRequestTemplate | undefined => {
-  const { proofRequestTemplates } = useConfiguration()
-  return useMemo(() => proofRequestTemplates?.find(template => template.id === templateId), [templateId])
+  const [proofRequestTemplate, setProofRequestTemplate] = useState<ProofRequestTemplate | undefined>(undefined)
+  const { proofTemplateBaseUrl } = useConfiguration()
+  const resolver = useRemoteProofBundleResolver(proofTemplateBaseUrl)
+  const acceptDevCredentials = false
+
+  useEffect(() => {
+    resolver.resolveById(templateId, acceptDevCredentials).then(template => {
+      if (template) {
+        setProofRequestTemplate(applyTemplateMarkers(template))
+      }
+    })
+  }, [])
+  return proofRequestTemplate
 }
