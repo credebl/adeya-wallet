d86ed69dcca19f55f750ffc205f561680b843e82
Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
2024-03-08 21:41:43 +0530

    feat: add url shortening (#173)
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>

--- app/utils/PushNotificationHelper.ts
+++ app/utils/PushNotificationHelper.ts
@@ -71,7 +71,7 @@ const _requestPermission = async (agent: AdeyaAgent): Promise<void> => {
 const _getMediatorConnection = async (agent: AdeyaAgent): Promise<ConnectionRecord | undefined> => {
   const connections = await getAllConnections(agent)
   for (const connection of connections) {
-    if (connection.theirLabel === Config.MEDIATOR_LABEL) {
+    if (connection.theirLabel?.toUpperCase() === Config.MEDIATOR_LABEL) {
       return connection
     }
   }
