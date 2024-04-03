9b5887ac90f511a02831475c90f6fa0883447442
Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
2024-04-03 23:28:08 +0530

    feat: add polygon module (#179)
    * feat: add polygon module wip
    
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
    
    * refactor: upgrade adeya-sdk
    
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
    
    ---------
    
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  overrides: [
+    {
+      test: './node_modules/ethers',
+      plugins: [['@babel/plugin-transform-private-methods', { loose: true }]],
+    },
+  ],
 }
