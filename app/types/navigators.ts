46edd46fbc472d92ef77d466c1b1a2b02f356e6c
Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
2024-07-01 16:51:41 +0530

    feat: add html rendering support
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>

--- app/types/navigators.ts
+++ app/types/navigators.ts
@@ -51,6 +51,7 @@ export enum Screens {
   DataRetention = 'Data Retention',
   Explore = 'Explore',
   OrganizationDetails = 'Organization Details',
+  RenderCertificate = 'Render Certificate',
 }
 
 export enum Stacks {
@@ -144,6 +145,7 @@ export type CredentialStackParams = {
   [Screens.Credentials]: undefined
   [Screens.CredentialDetails]: { credential: CredentialExchangeRecord }
   [Screens.CredentialDetailsW3C]: { credential: W3cCredentialRecord }
+  [Screens.RenderCertificate]: { credential: Pick<W3cCredentialRecord, 'credential'> }
   [Screens.Scan]: undefined
 }
 export type OrganizationStackParams = {
